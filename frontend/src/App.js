9097c79b950e46efba3065ae1be706e7e3596744
ravshan <ravshan.sher23@yandex.ru>
2023-02-07 12:43:29 +0300

    lesson_2 Ready

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,62 @@
 import React from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
+import UsersList from './components/Users';
+import MenuList from './components/Menu';
+import FooterList from './components/Footer';
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-        'users': []
+        'users': [],
+        'menus': [],
+        'footers': []
   }
 }
+
+componentDidMount() {
+  const menus = [
+    {
+      'name': 'All users',
+    },
+    {
+      'name': 'Project',
+    },
+    {
+      'name': 'TODO',
+    }
+  ]
+  const footers = [
+    {
+      'name': 'Created',
+    },
+    {
+      'name': 'Contact',
+    }
+  ]
+    axios.get('http://127.0.0.1:8000/api/users/').then(response => {
+      this.setState(
+        {
+          'users': response.data,
+          'menus': menus,
+          'footers': footers
+
+        }
+      )
+    }).catch(error => console.log(error))
+    
+
+  
+}
+
 render () {
   return (
       <div>
-          Main App
+          <MenuList menus={this.state.menus}/>
+          <UsersList users={this.state.users}/>
+          <FooterList footers={this.state.footers}/>
       </div>
         );
 }
