31883bdc677a13477ec6e2728e57730638652263
Sherzhanov Ravshan <101931673+ravshansher23@users.noreply.github.com>
2023-03-21 16:51:20 +0300

    Lesson10 ready (#10)

--- config/urls.py
+++ config/urls.py
@@ -8,7 +8,7 @@ from users.views import CustomUserModelViewSet
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-
+from graphene_django.views import GraphQLView
 from todo.views import TODOCustomViewSet, ProjectCustomViewSet
 
 router = DefaultRouter()
@@ -36,7 +36,7 @@ urlpatterns = [
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-
+    path("graphql/", GraphQLView.as_view(graphiql=True)),
     path('api-token-auth/', views.obtain_auth_token),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
