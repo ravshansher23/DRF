4d82b6867554f6b4471d063ff274129b4bd0959d
ravshan <ravshan.sher23@yandex.ru>
2023-03-22 18:40:22 +0300

    Lesson12 ready

---
+++ wait-for-postgres.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# wait-for-postgres.sh
+set -e
+host="$1"
+shift
+cmd="$@"
+until PGPASSWORD="admin123456" psql -h "$host" -d "todo" -U "admin" -c '\q';>
+>&2 echo "Postgres is unavailable - sleeping"
+sleep 1
+done
+>&2 echo "Postgres is up - executing command"
+exec $cmd
